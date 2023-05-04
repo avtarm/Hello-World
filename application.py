c541af51c0fbc1974c804871c6e49419f3fc02cd
avtarm <42239385+avtarm@users.noreply.github.com>
2023-05-04 23:06:07 +0100

    Update application.py

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
  
 @app.route('/')
 def index():
-    return 'Hello, this is a sample application which has been through a CICD process, then deployed to an Openshift Cluster on IBM Cloud, using GitOps! That is what I call an automated process! The developer will update the code and it will automatically be published'!'
+    return 'Hello, this is a sample application which has been through a CICD process, then deployed to an Openshift Cluster on IBM Cloud, using GitOps! That is what I call an automated process! The developer will update the code and it will automatically be published!'
  
 # if __name__ == "__main__":
 app.run(host='0.0.0.0', debug=True, port=8080)
