bc2bc6a5c98be769099115aa9ecdf18eea92d446
avtarm <42239385+avtarm@users.noreply.github.com>
2023-05-04 17:27:42 +0100

    Update application.py

--- application.py
+++ application.py
@@ -1,10 +1,12 @@
-from flask import Flask
+# from flask import Flask
  
-app = Flask(__name__)
+# app = Flask(__name__)
  
-@app.route('/')
-def index():
-    return 'Hello World!'
+# @app.route('/')
+# def index():
+#     return 'Hello World!'
  
-# if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=8080)
+# # if __name__ == "__main__":
+#     app.run(host='0.0.0.0', debug=True, port=8080)
+
+print("hello my name is jeff")
