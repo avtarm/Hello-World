5691b397884a9172195545ca2092d6034989a4c0
avtarm <42239385+avtarm@users.noreply.github.com>
2023-05-04 17:16:26 +0100

    Update hello-world-application.py

--- hello-world-application.py
+++ hello-world-application.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
  
 @app.route('/')
 def index():
-    return 'This is a hello world application'
+    return 'Hello World!'
  
-# if __name__ == "__master__":
+if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=8080)
